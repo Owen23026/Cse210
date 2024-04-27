9724d2e2abc35c1dc41c3c7836b3779409ce7568
Lore5775 <paralleluni32@gmail.com>
2024-04-27 10:45:00 -0600

    Finish both Prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        String grstr = null;
+        Console.Write("What was your Grade? ");
+        int grade = int.Parse(Console.ReadLine());
+        if(grade > 100 || grade < 0){
+            Console.WriteLine("Grade is invalid");
+            //Id like to learn about error handling and OS interfacing with C#
+            System.Environment.Exit(404);
+        }
+        switch(grade / 10)
+        {
+            case 10:
+                grstr = "an A";
+            break;
+            case 9:
+                grstr = "an A";
+            break;
+            case 8:
+                grstr = "a B";
+            break;
+            case 7:
+                grstr = "a C";
+            break;
+            case 6:
+                grstr = "a D";
+            break;
+            default:
+                grstr = "an F";
+            break;
+        }
+        if(grade >= 60 && grade <= 96){
+            //This might help performance for cpu, but hurt memory performance slightly
+            int x = grade%10;
+            if(x >= 7){
+                grstr = grstr + "+";
+            }else if(x <= 3){
+                grstr = grstr + "-";
+            }
+        }
+        Console.WriteLine("Your grade is " + grstr);
     }
 }
---
+++ sandbox/Testing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Internal;
+
+namespace Testing;
+
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        String ctest = "hello";
+        Console.WriteLine(ctest);
+    }
+}
