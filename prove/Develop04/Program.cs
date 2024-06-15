6e89a69b2934b98ef703c58cd44cff1f97171733
Owen23026 <bla23026@byui.edu>
2024-06-14 20:31:41 -0600

    Add program.cs Description

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -2,6 +2,11 @@ using System;
 
 class Program
 {
+
+    /*
+    I implemented a stretching activity for the stretch challenge that uses stretches from wii fit. It is not the greatest in terms of cleanliness,
+    , and it would have been a good idea to automate the poses and use a specialized object as well.
+    */
     static void Main(string[] args)
     {
         Console.Clear();
